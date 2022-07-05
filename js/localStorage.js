ee33e9a57d0c3dbc5e5da370351bd823c8e75872
Emanuel Martinez <yinproducciones@gmail.com>
2022-07-04 22:56:42 -0300

    segundo intento de curso de javascript

---
+++ js/localStorage.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', e => {
+    fetchData()
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        pintarCarrito()
+    }
+});
+
